99ce3614138ab8f515191018aa26d51cf143c12d
Ayesh Rodrigo <ayeshrodz@gmail.com>
2024-09-21 09:46:32 +1200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-diary-app/sw.js', { scope: '/my-diary-app/' })})}
